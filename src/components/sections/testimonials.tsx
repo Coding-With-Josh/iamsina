91cdc71d53544c3bbe6ccec6ca1e38b978e2d9d4
Coding-With-Josh <codewithjoshh@gmail.com>
2025-08-03 04:08:54 +0100

    wdwedwww

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -11,27 +11,29 @@ const tweetIds = [
 
 export default function TestimonialSection() {
   return (
-    <section id="testimonial" className="relative max-w-full h-full bg-gradient-to-b from-green-100 to-green-200 py-24 z-[-30] rounded-t-[48px]">
-      <div className="px-2">
-        <div className="pb-12">
+    <section id="testimonial" className="z-[-1] flex flex-col items-center justify-center relative w-full bg-gradient-to-b from-green-100 to-green-200 py-24 rounded-t-[48px]">
+      <div className="w-full px-4">
+        <div className="pb-12 w-full">
           <div className="flex w-full flex-col items-center justify-center pb-12 pt-14">
             <h3>
               <span className="tracking-tight mt-8 font-sans text-black text-4xl sm:text-5xl lg:text-6xl font-bold">
                 Join the club
               </span>
             </h3>
-            <p className="mt- text-lg text-neutral-400 text-center max-w-2xl">
+            <p className="mt-4 text-lg text-neutral-400 text-center max-w-2xl">
               Of-course I deliver premium quality designs. Just ask!
             </p>
           </div>
-          <div className="flex items-center justify-center w-full">
-            <div className="columns-1 sm:columns-2 md:columns-3 max-w-7xl px-4 gap-4 [column-fill:balance]">
-  {tweetIds.map((tweetId, i) => (
-    <div key={i} className="break-inside-avoid w-full overflow-hidden rounded-xl">
-      <Tweet id={tweetId} />
-    </div>
-  ))}
-</div>
+          <div className="w-full overflow-x-hidden">
+            <div className="columns-1 sm:columns-2 md:columns-3 max-w-7xl mx-auto gap-4 [column-fill:balance]">
+              {tweetIds.map((tweetId, i) => (
+                <div key={i} className="break-inside-avoid w-full overflow-hidden rounded-xl">
+                  <div className="p-2">
+                    <Tweet id={tweetId} />
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>    
